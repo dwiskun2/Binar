9608115c068f6c3e34d605ceeafbf6e78f8bc889
Your Name <dwisoepriadi@gmail.com>
2020-05-15 00:39:20 +0700

    push ke github

---
+++ Basic_Class/ProductsApp/controllers/product.controller.js
@@ -0,0 +1,15 @@
+exports.product_create = function (req, res) {
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
---
+++ Basic_Class/ProductsApp/routes/product.route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+const product_controller = require('../controllers/product.controller');
+
+
+router.get('/test', product_controller.test);
+router.get('/belajar',product_controller.product_create)
+module.exports = router;
+
+
